dee6995415827588334000ba29402983e632bd93
Louis Digon <digon@wisc.edu>
2024-04-24 19:00:29 -0500

    Added script that allows user controller to select a block and drag like real-life Jenga. Need to add to each block or use some other alternative.

---
+++ DraggableBlock.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class DraggableBlock : XRGrabInteractable
+{
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        SetGripOrientation(args.interactorObject);
+    }
+
+    private void SetGripOrientation(IXRSelectInteractor interactor)
+    {
+        if (interactor is XRDirectInteractor)
+        {
+            matchAttachmentPoints = true;
+            attachTransform.position = interactor.attachTransform.position;
+            attachTransform.rotation = interactor.attachTransform.rotation;
+        }
+        else
+        {
+            matchAttachmentPoints = false;
+        }
+    }
+}
+
